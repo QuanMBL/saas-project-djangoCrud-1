705defb7b7e046869485d35044b93db6c64df288
QuanMBL <162358434+QuanMBL@users.noreply.github.com>
2025-09-03 16:53:46 +0700

    setting again 1 railway

--- src/cfehome/cfehome/settings.py
+++ src/cfehome/cfehome/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-6-_w2bsu73!2wkbh3tt$+@m12*iu0#1b_bw%&!pzwf%q-%vae4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     ".railway.app" #https://saas.prod.railway.app
