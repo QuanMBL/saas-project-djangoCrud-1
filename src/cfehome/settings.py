53cc5651c05837155671d04e103b40d149a9d057
QuanMBL <162358434+QuanMBL@users.noreply.github.com>
2025-09-03 17:16:12 +0700

    done set up on railway

--- src/cfehome/settings.py
+++ src/cfehome/settings.py
@@ -26,13 +26,10 @@ SECRET_KEY = 'django-insecure-6-_w2bsu73!2wkbh3tt$+@m12*iu0#1b_bw%&!pzwf%q-%vae4
 DEBUG = False
 
 ALLOWED_HOSTS = [
-    ".railway.app" #https://saas.prod.railway.app
+    ".railway.app",
+    "localhost",
+    "127.0.0.1"
 ]
-if DEBUG:
-    ALLOWED_HOSTS +=[
-        "127.0.0.1",
-        "localhost"
-    ]
 
 
 # Application definition
