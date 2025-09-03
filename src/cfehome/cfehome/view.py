663fa4357c24a2fd9d468dbac0442a12d3934389
QuanMBL <162358434+QuanMBL@users.noreply.github.com>
2025-09-03 15:00:09 +0700

    Update views to change order

--- src/cfehome/cfehome/view.py
+++ src/cfehome/cfehome/view.py
@@ -12,7 +12,7 @@ def home_page_view(request,*args, **kwargs):
     
     # objects ở đây cho tương tác với database
     queryset= PageVist.objects.all() # tổng thể      vào trang bao nhiêu lần
-    
+    html_templates= "home.html"
     #số lần vào trang request 
     qs= PageVist.objects.filter(path=request.path)
     my_context = {
@@ -22,7 +22,7 @@ def home_page_view(request,*args, **kwargs):
     }
 
    
-    html_templates= "home.html"
+    
     PageVist.objects.create()
     # yêu cầu, trang được trả về, dữ liệu sẽ trả về cho trang
     return render(request,html_templates,my_context) 
