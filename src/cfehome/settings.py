c1350d8e31bfbdf6d42e5db513a9a0ec107f4e09
QuanMBL <162358434+QuanMBL@users.noreply.github.com>
2025-09-03 17:50:30 +0700

    Added Django DEBUG

--- src/cfehome/settings.py
+++ src/cfehome/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -23,7 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-6-_w2bsu73!2wkbh3tt$+@m12*iu0#1b_bw%&!pzwf%q-%vae4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = str(os.environ.get("DEBUG")).lower() == "true" # Lấy giá trị biến môi trường DEBUG của hệ điều hành.
+
+print("DEBUG",DEBUG,type(DEBUG))
 
 ALLOWED_HOSTS = [
     ".railway.app",
